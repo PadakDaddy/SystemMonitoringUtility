ebbc95ec1ea2ecbb1374b0c74b3cdad62a388250
padakdaddy <zzangism@gmail.com>
2026-01-22 10:02:47 -0500

    Create SetupTimer Method

--- SysMonitor/Views/MainWindow.cs
+++ SysMonitor/Views/MainWindow.cs
@@ -6,16 +6,36 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using SystemMonitor.Services;
+
 
 namespace SystemMonitor.Views
 {
     public partial class MainWindow : Form
     {
+        //Timer for updating data every 1 second.
+        private System.Windows.Forms.Timer updateTimer;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void SetupTimer()
+        {
+            // create Timer
+            updateTimer = new System.Windows.Forms.Timer();
+
+            //Interval 1 second
+            updateTimer.Interval = 1000;
+
+            updateTimer.Tick += UpdateTimer_Tick;
+
+            //Timer start
+            updateTimer.Start();
+        }
+                
 
         private void systemInfoGB_Enter(object sender, EventArgs e)
         {
